aa06e958c8a30259623c8ac55eb7cd948fa1df5a
Utsavraut <raututsav35@gmail.com>
2021-12-01 12:47:27 +0545

    labwork

---
+++ Lab 2.py
@@ -0,0 +1,5 @@
+#area of triangle
+b=int(input("Enter the base in cm:"))
+h=int(input("Enter the height in cm:"))
+Area=1/2*b*h
+print("The area of traingle is:",Area,"cm\u00b2")
--- area of triangle.py
+++
@@ -1,4 +0,0 @@
-b=int(input("Enter the base:"))
-h=int(input("Enter the height:"))
-A=1/2*b*h
-print("The area of traingle is:"+str(A))
