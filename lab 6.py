96fb1ce63a772edc89d9a4590ddf796f95915464
Utsavraut <raututsav35@gmail.com>
2021-12-01 13:16:43 +0545

    labwork

---
+++ lab 6.py
@@ -0,0 +1,4 @@
+# sum of first ten odd numbers
+list=[1,3,5,9,11,13,15,17,19,21]
+sum = sum(list)
+print (sum)
---
+++ lab5.py
@@ -0,0 +1,4 @@
+#sum of first 10 even numbers
+list=[2,4,6,8,10,12,14,16,18,20]
+sum = sum(list)
+print(sum)
