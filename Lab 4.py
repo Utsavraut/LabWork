e5b1411aae926af3300157b71aae6b15eada7e46
Utsavraut <raututsav35@gmail.com>
2021-12-07 21:33:21 +0545

    Day 4

---
+++ Lab 4.py
@@ -0,0 +1,6 @@
+# Write a program to print absolute value of a number entered by user. E.g.-
+# INPUT: 1        OUTPUT: 1
+# INPUT: -1        OUTPUT: 1
+number = int(input("Enter a number:")) 
+number=abs(number)
+print("Number:{}".format(number))
