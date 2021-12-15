cb80c306486f7adf5e5a8784c3e9399b8b5417cb
Utsavraut <raututsav35@gmail.com>
2021-12-15 13:23:50 +0545

    Day 5

--- Day 5/Lab 4.py
+++ Day 5/Lab 4.py
@@ -1,2 +1,5 @@
 # Write a Python program to construct the following pattern, using a nested for loop.* * ** * * * * * * * * * * * * * * * * * * * * *
-
+for i in range(6):
+    print("* " * (i+1))
+for w in range(6,0,-1):
+    print("* "*(w-1))
